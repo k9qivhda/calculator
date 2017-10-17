d4727edca317dcf41d50eb722caacf07f863fdff
DESKTOP-IKENPIJ\cskim <csdany@gmail.com>
2017-10-17 14:46:40 +0900

    conflict2

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(int argc, char* argv[])
 	int a, b, c;
 	char operator;
 	
-	printf("Please enter a expression");
+	printf("수식을 입력하세요:");
 	scanf("%d %c %d", &a, &operator, &b);
 	
 	switch (operator)
